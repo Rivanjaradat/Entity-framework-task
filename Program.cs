5ec2f04646f7f4991e5b64d90f6cb9c534a34a9c
Rivan jaradat <rivanr44@gmail.com>
2024-08-04 12:53:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EF__task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EF__task.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF__task.context
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+            optionsBuilder.UseSqlServer("Server=localhost;Database=ASP9_EF;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        public DbSet<product> Products { get; set; }
+    }
+}
